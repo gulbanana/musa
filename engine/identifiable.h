a0293659fceed815a9b985b5ec2a80837b17033b
Thomas Castiglione <castiglione@gmail.com>
2012-09-28 22:44:06 +0800

    4space tabs

--- engine/identifiable.h
+++ engine/identifiable.h
@@ -7,28 +7,28 @@ public:                   \
         __VA_ARGS__       \
     }
 
-#define IDENTIFIABLE_COLLECTION(itype, iname, iplural)	\
-public:													\
-	template<typename T>								\
-	void add_##iname(std::unique_ptr<T> iname)			\
-	{													\
-		iplural[T::ID] = std::move(iname);				\
-	}													\
-														\
-	template<typename T>								\
-	inline bool has_##iname()							\
-	{													\
-		return has_##iname(T::ID);						\
-	}													\
-														\
-	bool has_##iname(itype::ID id)						\
-	{													\
-		return iplural.count(id) > 0;					\
-	}													\
-														\
-	template<typename T> T* get_##iname()				\
-	{													\
-		return static_cast<T*>(iplural[T::ID].get());	\
-	}													\
-protected:												\
-	std::map<itype::ID, std::unique_ptr<itype>> iplural	
+#define IDENTIFIABLE_COLLECTION(itype, iname, iplural) \
+public:                                                \
+    template<typename T>                               \
+    void add_##iname(std::unique_ptr<T> iname)         \
+    {                                                  \
+        iplural[T::ID] = std::move(iname);             \
+    }                                                  \
+                                                       \
+    template<typename T>                               \
+    inline bool has_##iname()                          \
+    {                                                  \
+        return has_##iname(T::ID);                     \
+	}                                                  \
+                                                       \
+    bool has_##iname(itype::ID id)                     \
+    {                                                  \
+        return iplural.count(id) > 0;                  \
+    }                                                  \
+                                                       \
+    template<typename T> T* get_##iname()              \
+    {                                                  \
+        return static_cast<T*>(iplural[T::ID].get());  \
+    }                                                  \
+protected:                                             \
+    std::map<itype::ID, std::unique_ptr<itype>> iplural	
