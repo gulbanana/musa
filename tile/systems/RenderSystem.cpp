59b229bb339622d58aa80ac490a790f735739d0a
Thomas Castiglione <castiglione@gmail.com>
2013-09-30 17:29:53 +0100

    render within a camera viewport

--- tile/systems/RenderSystem.cpp
+++ tile/systems/RenderSystem.cpp
@@ -16,7 +16,12 @@ std::vector<IComponent::ID> RenderSystem::required_components() const
     return IComponent::require<CDraw, CTransform>();
 }
 
-RenderSystem::RenderSystem(IBlitter* blitter, shared_ptr<GameState> state) : _blitter(blitter), _state(state) {}
+RenderSystem::RenderSystem(IBlitter* blitter, shared_ptr<GameState> state) : _blitter(blitter), _state(state) 
+{
+    _cam_centre_x = 0;
+    _cam_centre_y = 0;
+    _cam_z = 0;
+}
 
 RenderSystem::~RenderSystem() {}
 
@@ -25,12 +30,25 @@ void RenderSystem::on_frame_start()
 	_tram = _blitter->get_tram();
 
 	_tram->fill(tile('.'));
+
+    _cam_left = _cam_centre_x - _tram->width/2;
+    _cam_bottom = _cam_centre_y - _tram->height/2;
+    _cam_right = _cam_left + _tram->width;
+    _cam_top = _cam_bottom + _tram->height;
 }
 
 void RenderSystem::on_frame_entity(std::shared_ptr<IEntity> e)
 {
     auto transform = e->get_component<CTransform>();
-    auto drawable = e->get_component<CDraw>();
+    
+    if (!within_camera_bounds(transform->translate)) return;
 
-    _tram->draw(drawable->layout, transform->translate.x, transform->translate.y);
+    auto drawable = e->get_component<CDraw>();
+    _tram->draw(drawable->layout, transform->translate.x - _cam_left, transform->translate.y - _cam_bottom);
+}
+
+bool RenderSystem::within_camera_bounds(point p)
+{
+    return p.x >= _cam_left && p.y >= _cam_bottom &&
+           p.x <= _cam_right && p.y <= _cam_top;
 }
--- tile/systems/RenderSystem.h
+++ tile/systems/RenderSystem.h
@@ -10,6 +10,17 @@ private:
 	std::shared_ptr<GameState> _state;
     grid* _tram;
 
+    int _cam_centre_x;
+    int _cam_centre_y;
+    int _cam_z;
+
+    int _cam_top;
+    int _cam_bottom;
+    int _cam_left;
+    int _cam_right;
+
+    bool within_camera_bounds(point);
+
 private:
 	std::vector<ISystem::ID> required_systems() const override;
     std::vector<IComponent::ID> required_components() const override;
